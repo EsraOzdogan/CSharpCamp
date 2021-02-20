a6599f1cc7033b6fd832a4a7f58045df2ba4c4d6
Esra Özdoğan <esraozdogaan@gmail.com>
2021-02-20 23:30:27 +0300

    Mathematics

---
+++ Mathematics/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Mathematics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            fourOperations dortIslem = new fourOperations();
+            dortIslem.Topla(5, 6);
+
+            dortIslem.Topla(6, 9);
+        }
+    }
+}
