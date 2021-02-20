bd6af171844861c4e3decba5b1a3fa9f8dc4bef5
Esra Özdoğan <esraozdogaan@gmail.com>
2021-02-20 23:30:41 +0300

    Methods

---
+++ Methods/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace Methods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string urunAdi = "Elma";
+            double fiyati = 15;
+            string aciklama = "Amasya elması";
+
+
+            string[] meyveler = new string[] { "Elma", "Karpuz" };
+
+            Product urun1 = new Product();
+            urun1.Adi = "Elma";
+            urun1.Fiyati = 15;
+            urun1.Aciklama = "Amasya elması";
+
+            Product urun2 = new Product();
+            urun2.Adi = "Karpuz";
+            urun2.Fiyati = 80;
+            urun2.Aciklama = "Diyarbakır karpuzu";
+
+            Product[] urunler = new Product[] { urun1, urun2 };
+
+            //type-safe -- tip güvenli
+            foreach (var urun in urunler)   //2 kere dönücek 2 ürün var. urunler dizi, urun her bir elemanı tanımlamak için değişken
+            {
+                Console.WriteLine(urun.Adi);
+                Console.WriteLine(urun.Fiyati);
+                Console.WriteLine(urun.Aciklama);
+                Console.WriteLine("--------------------");
+            }
+
+            Console.WriteLine("------------Metotlar---------------");
+            //instance - örnek
+            //encapsulation
+
+            SepetManager sepetManager = new SepetManager();
+            sepetManager.Ekle(urun1);
+            sepetManager.Ekle(urun2);
+
+            sepetManager.Ekle2("Armut", "Yeşil armut", 12, 10);    //encapsulation- ayrı ayrı yazılanların bir düzene girmesi
+            sepetManager.Ekle2("Elma", "Yeşil elma", 12, 9);
+            sepetManager.Ekle2("Karpuz", "Diyarbakır karpuzu", 12, 8);
+
+
+        }
+    }
+}
+
+//Dont repeat yourself - DRY - Clean Code - Best Practice
+
