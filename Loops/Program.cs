47d01832af825f2ea871bda845020d328be6a57b
Esra Özdoğan <esraozdogaan@gmail.com>
2021-02-20 18:04:02 +0300

    Loops

---
+++ Loops/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Loops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string kurs1 = "Yazılım Geliştirici Yetiştirme Kampı";
+            string kurs2 = "Programlamaya başlangıç için temel kurs";
+            string kurs3 = "Java";
+            string kurs4 = "Python";
+            string kurs5 = "C++";
+
+            Console.WriteLine(kurs1);
+            Console.WriteLine(kurs2);
+            Console.WriteLine(kurs3);
+            Console.WriteLine(kurs4);
+            Console.WriteLine(kurs5);
+
+            //array - dizi
+
+            string[] kurslar = new string[] { kurs1,
+                "Programlamaya başlangıç için temel kurs",
+                "Java", "Python", "C#" };
+
+           /* for (int i = 0; i < 10; i++)  //i=i+2
+            {
+                Console.WriteLine(i);
+            }*/
+
+            for (int i = 0; i < kurslar.Length; i++)  //i=i+2
+            {
+                Console.WriteLine(kurslar[i]);
+            }
+
+            Console.WriteLine("For bitti");
+
+            foreach (string kurs in kurslar)  //kursları dolaş, foreach dizileri dolaşır.Yukardaki for ile aynı işlemi yapıyor.
+            {
+                Console.WriteLine(kurs); 
+            }
+
+            Console.WriteLine("sayfa sonu - footer");
+        }
+    }
+}
