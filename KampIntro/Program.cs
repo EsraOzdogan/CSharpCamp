3602844d248a39be34c8978bbc6a12db4117bde0
Esra Özdoğan <esraozdogaan@gmail.com>
2021-02-20 18:03:51 +0300

    Intro

--- KampIntro/Program.cs
+++ KampIntro/Program.cs
@@ -6,7 +6,38 @@ namespace KampIntro
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string kategoriEtiketi = "Kategoriler";
+            int ogrenciSayisi = 32000;
+            double faizOrani = 1.45;  //ondalıklı sayı
+            bool sistemeGirisYapmisMi = true;  //bool true/false   //burası veri kaynağı aslında
+            double dolarDun = 7.55;
+            double dolarBugun = 7.55;
+
+            if (dolarDun > dolarBugun)
+            {
+                Console.WriteLine("Azalış butonu");
+            }
+            else if (dolarDun < dolarBugun)
+            {
+                Console.WriteLine("Artış butonu");
+            }
+            else
+            {
+                Console.WriteLine("değişmedi butonu");
+            }
+
+
+
+            if (sistemeGirisYapmisMi == true)
+            {
+                Console.WriteLine("Kullanıcı ayaları butonu");
+            }
+            else
+            {
+                Console.WriteLine("Giriş yap butonu");
+            }
+
+            Console.WriteLine(kategoriEtiketi);
         }
     }
 }
