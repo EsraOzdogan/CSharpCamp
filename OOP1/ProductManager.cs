1bad72877133e700c51f3f885a2ad8f9c6317fb3
EsraOzdogan <esraozdogaan@gmail.com>
2021-04-02 04:07:25 +0300

    OOP1

---
+++ OOP1/ProductManager.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP1
+{
+    class ProductManager
+    {
+        //encapsulation
+        public void Add(Product product)//101         //Sonucunda bir şey yapılmadığı için void
+        {
+            //product.ProductName = "Kamera";
+            Console.WriteLine(product.ProductName + " added.");
+        }
+
+        public void Update(Product product)
+        {
+            Console.WriteLine(product.ProductName + " updated.");
+        }
+       /* public  void BiseyYap(int sayi)//100
+        {
+            sayi = 99;
+        }*/
+
+        /*public int Topla(int sayi1, int sayi2)
+        {
+            return sayi1 + sayi2;
+        }
+        public void Topla2(int sayi1, int sayi2)
+        {
+            Console.WriteLine(sayi1+ sayi2);
+        }*/
+    }
+}
---
+++ OOP1/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace OOP1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //classlar 2ye ayrılır: özellik ve fonksiyon barındıranlar.fonksiyon olanlar 'managerlı'
+
+            Product product1 = new Product(); //101
+            product1.Id = 1;
+            product1.CategoryId = 2;
+            product1.ProductName = "Masa";
+            product1.UnitPrice = 500;
+            product1.UnitsInStock = 3;
+            //iki şekilde de yazılır.Gerçek hayatta böyle olmuyor.veriden kullanıcıdan vs geliyor.
+            Product product2 = new Product
+            {
+                Id = 2,
+                CategoryId = 5,
+                UnitsInStock = 5,
+                ProductName = "Kalem",
+                UnitPrice = 35
+            };
+
+            //PascalCase- ilk harfler büyük   //camelCase-ilk harf küçük diğer harf büyük
+            //case sensitive
+            ProductManager productManager = new ProductManager();
+            productManager.Add(product1);
+            Console.WriteLine(product1.ProductName); //kamera
+
+
+            /*int sayi = 100;
+            productManager.BiseyYap(sayi);
+            Console.WriteLine(sayi); //100*/
+
+
+            //productManager.Topla2(3,6);
+
+            //int toplamasonucu= productManager.Topla(3,6);
+            //Console.WriteLine( toplamasonucu*2);
+
+           
+            //int,double,bool... değer tip
+            //diziler,class, abstract class, interface ... referans tip
+            //ref out
+
+        }
+    }
+}
