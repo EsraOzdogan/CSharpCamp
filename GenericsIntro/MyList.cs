a26995ab2e1ec3c7e8f9c2a9e28e275fc8a881b5
EsraOzdogan <esraozdogaan@gmail.com>
2021-04-02 04:59:39 +0300

    generics intro

---
+++ GenericsIntro/MyList.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GenericsIntro
+{
+    class MyList<T>   //Mylist'te T ile çalışıcam.string,int vs olur. T type yani T olarak ne verirsem istediğim elemanın türü de odur.
+    {
+        T[] items; //array
+        //constructor
+        //ctor tab tab
+        public MyList()  //0 elemanlı dizi
+        {
+            items = new T[0];
+        }
+        //eleman sayısını 1 arttırmam gerekiyor.
+        public void Add(T item)  
+        {
+
+            T[] tempArray = items; //102
+            items = new T[items.Length + 1]; //103  //eleman sayısını 1 arttırmam gerekiyor. newlendiği için referan numarası gitti o yüzden geçici dizi oluştuurulaca temp diye.yukarda
+
+            for (int i = 0; i < tempArray.Length; i++)//temparraydeki değeri geri itemse atıyoruz
+            {
+                items[i] = tempArray[i];
+            }
+
+            items[items.Length - 1] = item;
+        }
+        public int Length
+        {
+            get { return items.Length; }
+        }
+
+        public T[] Items
+        {
+            get { return items; }
+        }
+    }
+}
---
+++ GenericsIntro/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GenericsIntro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyList<string> names = new MyList<string>(); //string,int vs istediğini verebilirsin T çünkü. Product da verilebilir.
+            names.Add("Engin"); // string olduğu için string değer verilecek.
+
+            List<string> list= new List<string>();
+            Console.WriteLine(list.Count);
+
+
+            Console.WriteLine(names.Length);
+
+            names.Add("Kerem");
+
+            Console.WriteLine(names.Length);
+
+            foreach (var name in names.Items)
+            {
+                Console.WriteLine(name);
+            }
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
