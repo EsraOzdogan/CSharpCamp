c394cf3949135a25559d52b80666545000912c4b
EsraOzdogan <esraozdogaan@gmail.com>
2021-04-06 19:32:04 +0300

    Class metot demo

---
+++ ClassMetotDemo/CustomerManager.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class CustomerManager
+    { 
+        public void Add(Customer customer)
+        {
+            Console.WriteLine("Customer Id : " + customer.Id + "  " +
+               "Customer No : " + customer.CustomerNo + "  " + "Customer First Name : " + customer.FirstName + "  " +
+               "Customer Last Name : " + customer.LastName + "  " +
+               "Customer City : " + customer.City + " " + "customer added");
+        }
+        public void Delete(Customer customer)
+        {
+
+            Console.WriteLine("Customer Id : " + customer.Id + "  " +
+               "Customer No : " + customer.CustomerNo + "  " + "Customer First Name : " + customer.FirstName + "  " +
+               "Customer Last Name : " + customer.LastName + "  " +
+               "Customer City : " + customer.City + " " + "customer deleted");
+        }
+
+
+        public void ListCustomer(Customer[] customers)
+        {
+            foreach (var customer in customers)
+            {
+                Console.WriteLine("Customer Id : " + customer.Id + "  " +
+              "Customer No : " + customer.CustomerNo + "  " + "Customer First Name : " + customer.FirstName + "  " +
+              "Customer Last Name : " + customer.LastName + "  " +
+              "Customer City : " + customer.City + " " );
+            }
+            Console.WriteLine("\n");
+        }
+
+      
+
+
+
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Esra Özdoğan
+            Customer customer1 = new Customer();
+            customer1.Id = 1;
+            customer1.CustomerNo = "123456";
+            customer1.FirstName = "Esra";
+            customer1.LastName = "Özdoğan";
+            customer1.City = "İstanbul";
+
+            Customer customer2 = new Customer();
+            customer2.Id = 2;
+            customer2.CustomerNo = "654321";
+            customer2.FirstName = "Ünal";
+            customer2.LastName = "Özdoğan";
+            customer2.City = "İstanbul";
+
+            CustomerManager customerManager = new CustomerManager();
+            customerManager.Add(customer1);
+            customerManager.Add(customer2);
+
+            Console.WriteLine(" \n");
+
+            Customer[] customers = new Customer[] { customer1, customer2 };
+            customerManager.ListCustomer(customers);
+
+            customerManager.Delete(customer1);
+            customerManager.Delete(customer2);
+
+
+            Console.ReadLine();
+        }
+    }
+}
